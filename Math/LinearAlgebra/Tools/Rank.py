fbc179c707ad56478578281c42a45e2167038913
Ebenezer Scrooge <1641367382@qq.com>
2022-03-09 18:16:00 +0800

    Update Rank.py

--- Math/LinearAlgebra/Tools/Rank.py
+++ Math/LinearAlgebra/Tools/Rank.py
@@ -4,29 +4,27 @@ from AdjugateAndInverse import upper_tri_mat
 from ElemTrans import generate_mat_by_et
 
 
-def enumerate_nz_sd(mat, utm=False):
+def enumerate_nz_sd(mat):
     queue = [mat]
     while len(queue) > 0:
         sub_mat = queue.pop(0)
         if numpy.linalg.det(sub_mat) != 0:
             return sub_mat.shape[0]
         else:
-            if utm:
-                new_mat = numpy.delete(sub_mat, sub_mat.shape[0] - 1, axis=0)
-                new_mat = numpy.delete(new_mat, 0, axis=1)
-                queue.append(new_mat)
-            else:
-                for i in range(0, sub_mat.shape[0]):
-                    for j in range(0, sub_mat.shape[0]):
-                        new_mat = numpy.delete(sub_mat, i, axis=0)
-                        new_mat = numpy.delete(new_mat, j, axis=1)
-                        queue.append(new_mat)
+            for i in range(0, sub_mat.shape[0]):
+                for j in range(0, sub_mat.shape[0]):
+                    new_mat = numpy.delete(sub_mat, i, axis=0)
+                    new_mat = numpy.delete(new_mat, j, axis=1)
+                    queue.append(new_mat)
     return 0
 
 
 def row_reduce(mat):
     mat_reduced = upper_tri_mat(mat, True)
-    rank = enumerate_nz_sd(mat_reduced, True)
+    rank = mat_reduced.shape[0]
+    for i in range(0, mat_reduced.shape[0]):
+        if not mat_reduced[i, :].any():
+            rank -= 1
     return rank
 
 
@@ -34,11 +32,11 @@ if __name__ == '__main__':
     mat_proto, test_mat = generate_mat_by_et(dim=5, et_count=10, rank=4)
     # test_mat = numpy.array(
     #     [
-    #         [0, 1, 0, 0, 0],
-    #         [11, 0, 1, 0, 0],
+    #         [108, 0, 0, 0, 0],
+    #         [0, 3, 0, 0, 0],
+    #         [0, 0, 0, 1, 0],
     #         [0, 0, 0, 0, 0],
-    #         [0, 0, 0, 0, 3],
-    #         [0, 0, 0, 2, 0]
+    #         [0, 0, 0, 0, 1]
     #     ]
     # )
     s1 = datetime.datetime.now()
