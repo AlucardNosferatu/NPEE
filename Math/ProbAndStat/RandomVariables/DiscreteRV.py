974b9b803f2cce127e12af1d97e6863fe3803465
Ebenezer Scrooge <1641367382@qq.com>
2022-03-10 10:17:09 +0800

    Update DiscreteRV.py

--- Math/ProbAndStat/RandomVariables/DiscreteRV.py
+++ Math/ProbAndStat/RandomVariables/DiscreteRV.py
@@ -1,9 +1,14 @@
+import numpy
 import tqdm
 import sympy
 import matplotlib
 import matplotlib.pyplot as plt
 
 matplotlib.use('TkAgg')
+
+
+def trunc(values, decs=0):
+    return numpy.trunc(values * 10 ** decs) / (10 ** decs)
 
 
 class RandomVar:
@@ -39,13 +44,18 @@ class RandomVar:
             self.dist_funcs = funcs
             self.dist_laws = self.get_laws_from_funcs()
 
-    def get_prob(self, x):
+    def get_prob(self, x, use_law=False):
         p = 0
         if self.rv_type == 'finite':
             for i in range(0, len(self.regions) - 1):
                 region = self.regions[i]
-                if region[0] <= x < region[1]:
-                    p += self.dist_laws[i]
+                if region[0] <= x:
+                    if use_law:
+                        p += self.dist_laws[i]
+                    if x < region[1]:
+                        if not use_law:
+                            p = self.dist_funcs[i]
+                        break
             if self.regions[-1][0] <= x < self.regions[-1][1]:
                 p = 1
         else:
@@ -54,19 +64,24 @@ class RandomVar:
             if len(self.cache) == 0:
                 self.cache.append(0.0)
             while self.regions.evalf(subs={n: n0}) <= x.evalf():
-                if len(self.cache) >= n0 + 1:
-                    pass
-                else:
-                    self.cache.append(self.cache[-1] + self.dist_laws.evalf(subs={n: n0}))
-                p = self.cache[n0]
-                if self.cache[n0] >= 1:
-                    self.cache[n0] = 1
-                    p = 1
-                    break
+                if use_law:
+                    if len(self.cache) >= n0 + 1:
+                        pass
+                    else:
+                        self.cache.append(
+                            self.cache[-1] + self.dist_laws.evalf(subs={n: n0}))
+                    p = self.cache[n0]
+                    if self.cache[n0] >= 1:
+                        self.cache[n0] = 1
+                        p = 1
+                        break
+                # endregion
                 n0 += 1
-
+            if not use_law:
+                p = self.dist_funcs.evalf(subs={n: n0 - 1})
         if type(p) is not float:
             p = float(p)
+        p = trunc(p, 8)
         return p
 
     def get_funcs_from_laws(self):
@@ -80,6 +95,7 @@ class RandomVar:
         else:
             n = list(self.dist_laws.free_symbols)[0]
             f = sympy.summation(self.dist_laws, (n, 1, n))
+            f = f.simplify()
             return f
 
     def get_laws_from_funcs(self):
@@ -98,23 +114,29 @@ class RandomVar:
         else:
             n = list(self.dist_funcs.free_symbols)[0]
             laws = sympy.difference_delta(self.dist_funcs, n, 1)
-            laws = laws.subs(n, n - 1)
+            laws = laws.subs(n, n - 1).simplify()
             return laws
 
 
 def test_drv_1():
     # m=2
     # a=2/(m*(m+1))=1/3
-    x = sympy.Symbol('x')
-    m = 100
-    f1 = x / sympy.E
-    f2 = 2 * x / (m * (m + 1))
+    n = sympy.Symbol('n')
+    m = 10
+    f1 = n / sympy.E
+    f2 = 2 * n / (m * (m + 1))
     rv = RandomVar(seg_p=[f1], laws=f2)
-    x = range(1, 100)
-    y = []
+    x = range(1, m)
+    y1 = []
+    y2=[]
     for i in tqdm.tqdm(x):
-        y.append(rv.get_prob(i / sympy.E))
-    plt.plot(x, y)
+        p1 = rv.get_prob(i / sympy.E, True)
+        y1.append(p1)
+    for i in tqdm.tqdm(x):
+        p2 = rv.get_prob(i / sympy.E, False)
+        y2.append(p2)
+    plt.plot(list(x), y1)
+    plt.plot(list(x), y2)
     plt.show()
 
 
@@ -132,13 +154,30 @@ def test_drv_3():
     sp1 = [-1, 0, 1]
     f1 = [0, 0.3, 0.8, 1]
     rv1 = RandomVar(seg_p=sp1, funcs=f1)
+    p1_1 = rv1.get_prob(0.5, True)
+    p1_2 = rv1.get_prob(0.5, False)
     x = sympy.Symbol('x')
     sp2 = [x / sympy.E]
     m = 100
     f2 = x * (x + 1) / (m * (m + 1))
     rv2 = RandomVar(seg_p=sp2, funcs=f2)
+    p2 = rv2.get_prob(5 / sympy.E, False)
+    print(p1_1, p1_2)
+    print(p2)
+
+
+def test_drv_4():
+    x = sympy.Symbol('x')
+    sp = [x / sympy.E]
+    m = 100
+    f1 = x * (x + 1) / (m * (m + 1))
+    rv1 = RandomVar(seg_p=sp, funcs=f1)
+    l1 = rv1.get_laws_from_funcs()
+    rv2 = RandomVar(seg_p=sp, laws=l1)
+    f2 = rv2.get_funcs_from_laws()
+    print(f2 == f1)
 
 
 if __name__ == '__main__':
-    test_drv_3()
+    test_drv_1()
     print('Done')
