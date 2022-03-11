b97aa8699e09d27f71bfd4f685cea6783cf12c9e
Ebenezer Scrooge <1641367382@qq.com>
2022-03-11 22:06:59 +0800

    Update IPC_1.py

--- 408/OS/Process/IPC/IPC_1.py
+++ 408/OS/Process/IPC/IPC_1.py
@@ -1,4 +1,6 @@
-from multiprocessing import shared_memory
+import time
+import uuid
+from multiprocessing import shared_memory, Pipe, Process
 
 
 def ipc_sm():
@@ -9,5 +11,33 @@ def ipc_sm():
     print('Done')
 
 
+def ipc_pipe():
+    (con1, con2) = Pipe(duplex=True)
+    pro1 = Process(target=ipc_pipe_p1, name='p1', args=(con1,))
+    pro2 = Process(target=ipc_pipe_p2, name='p2', args=(con2,))
+    pro1.start()
+    pro2.start()
+    input()
+    pro1.kill()
+    pro2.kill()
+
+
+def ipc_pipe_p1(pipe):
+    while True:
+        time.sleep(1)
+        pipe.send(['p1', str(uuid.uuid4())])
+        time.sleep(1)
+        print(pipe.recv())
+
+
+def ipc_pipe_p2(pipe):
+    time.sleep(0.5)
+    while True:
+        time.sleep(1)
+        print(pipe.recv())
+        time.sleep(1)
+        pipe.send(['p2', str(uuid.uuid4())])
+
+
 if __name__ == "__main__":
-    ipc_sm()
+    ipc_pipe()
