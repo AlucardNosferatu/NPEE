1387a08392e22915c837f6ce8c1f47811e0bba4d
Ebenezer Scrooge <1641367382@qq.com>
2022-03-19 09:47:33 +0800

    Update GoBackN.py

--- 408/Network/DataLinkLayer/FlowControl/GoBackN.py
+++ 408/Network/DataLinkLayer/FlowControl/GoBackN.py
@@ -1,8 +1,11 @@
+import json
 import threading
 import time
 import uuid
 
-from InfraStruct.MediaAgent import sr_interaction
+from flask import request
+
+from FlowControl.MediaAgent import sr_interaction, app
 
 
 class Sender:
@@ -97,3 +100,28 @@ class Sender:
                 if self.timer_table[i] > 10 * self.time_out:
                     print('timeout, resend old frame')
                     self.resend_old(i)
+
+
+sender_inst = None
+
+
+@app.route("/recv", methods=['POST'])
+def recv():
+    if request.method == "POST":
+        c_da = request.data
+        data = json.loads(c_da.decode())
+
+        return json.dumps(
+            {
+                '经典的错误': '标注的零分'
+            },
+            ensure_ascii=False
+        )
+
+
+if __name__ == '__main__':
+    app.run(
+        host="0.0.0.0",
+        port=int("20291"),
+        debug=True, threaded=True
+    )
