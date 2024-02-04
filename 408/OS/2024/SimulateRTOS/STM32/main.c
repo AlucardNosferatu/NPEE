32bbd8395e069d2128cdb2cfd805d3e16cb2b7a7
Ebenezer Scrooge <1641367382@qq.com>
2024-02-04 20:02:31 +0800

    Update

--- 408/OS/2024/SimulateRTOS/STM32/main.c
+++ 408/OS/2024/SimulateRTOS/STM32/main.c
@@ -1,20 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Sumjess
-  * @version V1.0
-  * @date    2019-09-xx
-  * @brief   MDK5.27
-  ******************************************************************************
-  * @attention
-  *
-  * 实验平台   :STM32 F429 
-  * CSDN Blog  :https://blog.csdn.net/qq_38351824
-  * 微信公众号 :Tech云
-  *
-  ******************************************************************************
-  */
-  
 /*
 *************************************************************************
 *                             包含的头文件
@@ -41,7 +24,6 @@ void delay_us(int time)
 		while(i--) ;
 	}
 }
-//毫秒级的延时
 void delay_ms(int time)
 {
 	int i=0;
@@ -78,6 +60,9 @@ static void BSP_Init(void)
 	GPIO_Init(GPIOA,&GPIO_InitStruct);
 	GPIO_InitStruct.GPIO_Pin=GPIO_Pin_2;
 	GPIO_Init(GPIOA,&GPIO_InitStruct);
+	GPIO_SetBits(GPIOA,GPIO_Pin_0);
+	GPIO_SetBits(GPIOA,GPIO_Pin_1);
+	GPIO_SetBits(GPIOA,GPIO_Pin_2);
 }
 /**********************************************************************
   * @ 函数名  ： LED_Task
@@ -87,62 +72,24 @@ static void BSP_Init(void)
   ********************************************************************/
 static void LED_Task(void* parameter)
 {
-	const int interval = 1000; //任务周期时间 --- 定义延时时间 ---  pdMS_TO_TICKS毫秒转tick
 	while (1)
 	{
-		vTaskDelay(interval);   /* 延时500个tick */
+		vTaskDelay(1000);   /* 延时500个tick */
 		GPIO_SetBits(GPIOA,GPIO_Pin_0);
-		vTaskDelay(interval);   /* 延时500个tick */
+		vTaskDelay(1000);   /* 延时500个tick */
 		GPIO_SetBits(GPIOA,GPIO_Pin_1);
-		vTaskDelay(interval);   /* 延时500个tick */
+		vTaskDelay(1000);   /* 延时500个tick */
 		GPIO_SetBits(GPIOA,GPIO_Pin_2);
 		
-		vTaskDelay(interval);   /* 延时500个tick */
+		vTaskDelay(1000);   /* 延时500个tick */
 		GPIO_ResetBits(GPIOA,GPIO_Pin_0);
-		vTaskDelay(interval);   /* 延时500个tick */
+		vTaskDelay(1000);   /* 延时500个tick */
 		GPIO_ResetBits(GPIOA,GPIO_Pin_1);
-		vTaskDelay(interval);   /* 延时500个tick */
+		vTaskDelay(1000);   /* 延时500个tick */
 		GPIO_ResetBits(GPIOA,GPIO_Pin_2);
   }
 }
-static TaskHandle_t AppTaskCreate_Handle = NULL;/* 创建任务句柄 */
 static TaskHandle_t LED_Task_Handle = NULL;/* LED_Task任务句柄 */
-/***********************************************************************
-  * @ 函数名  ： AppTaskCreate
-  * @ 功能说明： 为了方便管理，所有的任务创建函数都放在这个函数里面
-  * @ 参数    ： 无  
-  * @ 返回值  ： 无
-  **********************************************************************/
-static void AppTaskCreate(void)
-{
-	BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
-	GPIO_SetBits(GPIOA,GPIO_Pin_0);
-	GPIO_ResetBits(GPIOA,GPIO_Pin_1);
-	GPIO_ResetBits(GPIOA,GPIO_Pin_2);
-	taskENTER_CRITICAL();           //进入临界区
-	/* 创建LED_Task任务 */
-	xReturn = xTaskCreate((TaskFunction_t )LED_Task, /* 任务入口函数 */
-												(const char*    )"LED_Task",/* 任务名字 */
-												(uint16_t       )512,   /* 任务栈大小 */
-												(void*          )NULL,  /* 任务入口函数参数 */
-												(UBaseType_t    )2,      /* 任务的优先级 */
-												(TaskHandle_t*  )&LED_Task_Handle);/* 任务控制块指针 */
-	if(pdPASS == xReturn)
-	{
-		GPIO_SetBits(GPIOA,GPIO_Pin_0);
-		GPIO_SetBits(GPIOA,GPIO_Pin_1);
-		GPIO_ResetBits(GPIOA,GPIO_Pin_2);
-	}
-	else
-	{
-		GPIO_SetBits(GPIOA,GPIO_Pin_0);
-		GPIO_ResetBits(GPIOA,GPIO_Pin_1);
-		GPIO_ResetBits(GPIOA,GPIO_Pin_2);
-	}
-	vTaskDelete(AppTaskCreate_Handle); //删除AppTaskCreate任务
-	taskEXIT_CRITICAL();            //退出临界区
-}
-
 /*****************************************************************
   * @brief  主函数
   * @param  无
@@ -156,28 +103,18 @@ int main(void)
 	/* 开发板硬件初始化 */
 	BSP_Init();
 	BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
-	GPIO_SetBits(GPIOA,GPIO_Pin_0);
-	GPIO_ResetBits(GPIOA,GPIO_Pin_1);
-	GPIO_ResetBits(GPIOA,GPIO_Pin_2);
-	xReturn = xTaskCreate((TaskFunction_t )AppTaskCreate,  /* 任务入口函数---即任务函数的名称，需要我们自己定义并且实现。*/
-												(const char*    )"AppTaskCreate",/* 任务名字---字符串形式， 最大长度由 FreeRTOSConfig.h 中定义的configMAX_TASK_NAME_LEN 宏指定，多余部分会被自动截掉，这里任务名字最好要与任务函数入口名字一致，方便进行调试。*/
-												(uint16_t       )512,  /* 任务栈大小---字符串形式， 最大长度由 FreeRTOSConfig.h 中定义的configMAX_TASK_NAME_LEN 宏指定，多余部分会被自动截掉，这里任务名字最好要与任务函数入口名字一致，方便进行调试。*/
-												(void*          )NULL,/* 任务入口函数参数---字符串形式， 最大长度由 FreeRTOSConfig.h 中定义的configMAX_TASK_NAME_LEN 宏指定，多余部分会被自动截掉，这里任务名字最好要与任务函数入口名字一致，方便进行调试。*/
-												(UBaseType_t    )1, /* 任务的优先级---优先级范围根据 FreeRTOSConfig.h 中的宏configMAX_PRIORITIES 决定， 如果使能 configUSE_PORT_OPTIMISED_TASK_SELECTION，这个宏定义，则最多支持 32 个优先级；如果不用特殊方法查找下一个运行的任务，那么则不强制要求限制最大可用优先级数目。在 FreeRTOS 中， 数值越大优先级越高， 0 代表最低优先级。*/
-												(TaskHandle_t*  )&AppTaskCreate_Handle);/* 任务控制块指针---在使用内存的时候，需要给任务初始化函数xTaskCreateStatic()传递预先定义好的任务控制块的指针。在使用动态内存的时候，任务创建函数 xTaskCreate()会返回一个指针指向任务控制块，该任务控制块是 xTaskCreate()函数里面动态分配的一块内存。*/ 
-	/* 启动任务调度 */           
+	xReturn = xTaskCreate((TaskFunction_t )LED_Task, /* 任务入口函数 */
+												(const char*    )"LED_Task",/* 任务名字 */
+												(uint16_t       )256,   /* 任务栈大小 */
+												(void*          )NULL,  /* 任务入口函数参数 */
+												(UBaseType_t    )2,      /* 任务的优先级 */
+												(TaskHandle_t*  )&LED_Task_Handle);/* 任务控制块指针 */	/* 启动任务调度 */           
 	if(pdPASS == xReturn)
 	{
-		GPIO_SetBits(GPIOA,GPIO_Pin_0);
-		GPIO_SetBits(GPIOA,GPIO_Pin_1);
-		GPIO_ResetBits(GPIOA,GPIO_Pin_2);
 		vTaskStartScheduler();   /* 启动任务，开启调度 */
 	}
 	else
 	{
-		GPIO_SetBits(GPIOA,GPIO_Pin_0);
-		GPIO_ResetBits(GPIOA,GPIO_Pin_1);
-		GPIO_ResetBits(GPIOA,GPIO_Pin_2);
 		return -1;
 	}
 	while(1)   /* 正常不会执行到这里 */
