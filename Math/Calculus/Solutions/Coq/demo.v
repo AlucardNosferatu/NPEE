01c1e78b773fcce29f236b2747621316e5faf41e
Ebenezer Scrooge <1641367382@qq.com>
2024-02-26 20:31:22 +0800

    Update

--- Math/Calculus/Solutions/Coq/demo.v
+++ Math/Calculus/Solutions/Coq/demo.v
@@ -1,4 +1,9 @@
-Lemma silly : forall a b, a+b=b+a.
-Proof.
-  intros. induction a.
+Theorem plus_id_example : forall n m: nat,
+ n=m->
+ n+n=m+m.
 
+Proof.
+ intros n m.
+ intros A.
+ rewrite -> A.
+ reflexivity. Qed.
