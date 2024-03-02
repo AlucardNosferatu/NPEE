ace177f4b8d04619f1e82bdb1ee524f17e87d7dc
Ebenezer Scrooge <1641367382@qq.com>
2024-03-02 08:54:06 +0800

    Update demo.v

--- Math/Calculus/Solutions/Coq/demo.v
+++ Math/Calculus/Solutions/Coq/demo.v
@@ -1,9 +1,35 @@
-Theorem plus_id_example : forall n m: nat,
- n=m->
- n+n=m+m.
+Require Import Reals.
+Local Open Scope R_scope.
 
+Theorem jiaohuanlv : forall a b: R,
+ a+b=b+a.
+Proof. Admitted.
+
+Theorem jiehelv : forall a b c: R,
+ (a+b)+c=a+(b+c).
+Proof. Admitted.
+
+Theorem jiaohuanlv2 : forall x y z:R,
+ x+y+z=z+y+x.
 Proof.
- intros n m.
- intros A.
- rewrite -> A.
- reflexivity. Qed.
+ intros.
+ rewrite jiehelv.
+ rewrite (jiaohuanlv x (y+z)).
+ rewrite (jiaohuanlv y z).
+ reflexivity.
+Qed.
+
+ 
+Theorem jiaohuanlv3 : forall x y z:R,
+ x+y+z=z+y+x.
+Proof.
+ intros.
+ rewrite jiaohuanlv.
+ assert(H:x+y=y+x).
+ {
+  rewrite jiaohuanlv.
+  reflexivity.
+ }
+ rewrite H.
+ rewrite jiehelv.
+ reflexivity.
