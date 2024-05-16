d729f8e3038f1ad24e357d0571efdd4009d87e99
Ebenezer Scrooge <1641367382@qq.com>
2024-05-16 10:40:59 +0800

    Update

--- 408/OS/2024/ModularAutomation/safe_common_config.py
+++ 408/OS/2024/ModularAutomation/safe_common_config.py
@@ -1,8 +1,8 @@
-scan_host = '10.51.132.73'
+scan_host = '10.51.132.40'
 scan_host_web = 'http://{}/'.format(scan_host)
-target_name = 'EW300T'
-serial_number = 'MACCR229513T2'
-eweb_pass = 'LostXmas20291224'
-ssh_pass = '537f300ccabec155'
+target_name = 'BE72-PRO'
+serial_number = 'MACCBE7200Q18'
+eweb_pass = 'Ruijie123123'
+ssh_pass = '515d7d78c430f8ff'
 filename_bin = 'EW_3.0(1)B11P256_X30E-PRO_11162617_install_encypto.bin'
 folder = r'D:\RjDir\admin_d95c8884-f44c-4529-8f3b-48d5788371c1\Downloads\业务文档\安全扫描\R256-X30E\升级文件'
