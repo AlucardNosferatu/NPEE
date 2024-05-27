7a656bb8f7422b2dcd63af8f4bbd0b9aab094532
Ebenezer Scrooge <1641367382@qq.com>
2024-05-27 14:40:55 +0800

    Update Program.cs

--- 408/OS/2024/ModularAutomation/modules/rf_test/WT_SCPI_SampleCode/Program.cs
+++ 408/OS/2024/ModularAutomation/modules/rf_test/WT_SCPI_SampleCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -9,8 +10,37 @@ namespace WT_SCPI_SampleCode
 {
     class Program
     {
+        static WT_SCPI scpi;
+        static Dictionary<String, SignalDemod> demod_dict;
+
+
+
         static void Main(string[] args)
         {
+            scpi = new WT_SCPI();
+            demod_dict = new Dictionary<string, SignalDemod>();
+            demod_dict.Add(key: "Demod11ag", value: SignalDemod.Demod11ag);
+            demod_dict.Add(key: "Demod11b", value: SignalDemod.Demod11b);
+            demod_dict.Add(key: "Demod11n20", value: SignalDemod.Demod11n20);
+            demod_dict.Add(key: "Demod11n40", value: SignalDemod.Demod11n40);
+            demod_dict.Add(key: "Demod11ac20", value: SignalDemod.Demod11ac20);
+            demod_dict.Add(key: "Demod11ac40", value: SignalDemod.Demod11ac40);
+            demod_dict.Add(key: "Demod11ac80", value: SignalDemod.Demod11ac80);
+            demod_dict.Add(key: "Demod11ac160", value: SignalDemod.Demod11ac160);
+            demod_dict.Add(key: "Demod11ac8080", value: SignalDemod.Demod11ac8080);
+            demod_dict.Add(key: "DemodBluetooth", value: SignalDemod.DemodBluetooth);
+            demod_dict.Add(key: "DemodZigbee", value: SignalDemod.DemodZigbee);
+            demod_dict.Add(key: "DemodCW", value: SignalDemod.DemodCW);
+            demod_dict.Add(key: "Demod11ax20", value: SignalDemod.Demod11ax20);
+            demod_dict.Add(key: "Demod11ax40", value: SignalDemod.Demod11ax40);
+            demod_dict.Add(key: "Demod11ax80", value: SignalDemod.Demod11ax80);
+            demod_dict.Add(key: "Demod11ax160", value: SignalDemod.Demod11ax160);
+            demod_dict.Add(key: "Demod11ax8080", value: SignalDemod.Demod11ax8080);
+            demod_dict.Add(key: "Demod11be20", value: SignalDemod.Demod11be20);
+            demod_dict.Add(key: "Demod11be40", value: SignalDemod.Demod11be40);
+            demod_dict.Add(key: "Demod11be80", value: SignalDemod.Demod11be80);
+            demod_dict.Add(key: "Demod11be160", value: SignalDemod.Demod11be160);
+            demod_dict.Add(key: "Demod11be8080", value: SignalDemod.Demod11be8080);
             HttpListener listener = new HttpListener();
             listener.Prefixes.Add("http://localhost:20291/");
             listener.Start();
@@ -31,6 +61,10 @@ namespace WT_SCPI_SampleCode
                     {
                         Console.WriteLine($"{pair.Key}: {pair.Value}");
                     }
+                    if (jsonDict.ContainsKey("TASK"))
+                    {
+                        execute_task(jsonDict: jsonDict);
+                    }
                 }
                 catch (Exception)
                 {
@@ -40,8 +74,59 @@ namespace WT_SCPI_SampleCode
                 }
             }
         }
-        void execute_test() {
-            WT_SCPI scpi = new WT_SCPI();
+        static void execute_task(JObject jsonDict)
+        {
+            try
+            {
+                string task = jsonDict["TASK"].ToString();
+                switch (task)
+                {
+                    case "CONNECT":
+                        scpi.Connect(jsonDict["HOST"].ToString());
+                        break;
+                    case "UPLOAD":
+                        scpi.UploadWaveForm(jsonDict["BWV"].ToString());
+                        break;
+                    case "SET_DEVM":
+                        scpi.SetDevmParam(
+                            dutyRadio: int.Parse(jsonDict["DEVM_DUTY_RADIO"].ToString()),
+                            leadTime: double.Parse(jsonDict["DEVM_LEAD_TIME"].ToString()), 
+                            delayTime: double.Parse(jsonDict["DEVM_DELAY_TIME"].ToString())
+                            );
+                        break;
+                    case "VSG":
+                        scpi.Vsg(
+                            port: int.Parse(jsonDict["VSG_PORT"].ToString()),
+                            freqMHz: int.Parse(jsonDict["VSG_FREQ_MHZ"].ToString()),
+                            sampleRateMHz: int.Parse(jsonDict["VSG_SAMPLE_RATE_MHZ"].ToString()),
+                            packets: int.Parse(jsonDict["VSG_PACKETS"].ToString()),
+                            wave: jsonDict["VSG_WAVE"].ToString()
+                            );
+                        break;
+                    case "VSA":
+                        scpi.Vsa(
+                            port: int.Parse(jsonDict["VSA_PORT"].ToString()),
+                            freqMHz: int.Parse(jsonDict["VSA_FREQ_MHZ"].ToString()),
+                            sampleRateMHz: int.Parse(jsonDict["VSA_SAMPLE_RATE_MHZ"].ToString()),
+                            demod: demod_dict[jsonDict["VSA_DEMOD"].ToString()]
+                            );
+                        break;
+                    case "RESULT":
+                        string res = scpi.GetResult();
+                        File.WriteAllText(jsonDict["RESULT_TXT"].ToString(), res);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                scpi.Disconnect();
+            }
+        }
+        static void example_test() {
             try
             {
                 scpi.Connect("192.168.10.254");
