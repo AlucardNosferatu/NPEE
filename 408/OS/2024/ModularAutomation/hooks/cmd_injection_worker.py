f6413252add8ca1fca003edb8737072571d4d12f
Ebenezer Scrooge <1641367382@qq.com>
2024-07-02 17:58:52 +0800

    Update

--- 408/OS/2024/ModularAutomation/hooks/cmd_injection_worker.py
+++ 408/OS/2024/ModularAutomation/hooks/cmd_injection_worker.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 
 def h0(params):
@@ -8,13 +9,20 @@ def h0(params):
     params['eweb']['pass'] = params['wvt']['eweb_pass']
     pass_dict = {"type": "noenc", "password": params['eweb']['pass']}
     pass_str = json.dumps(obj=pass_dict)
-    params['console']['send_string'] = "ac_config set -m eweb_password '{}'".format(pass_str)
+    cmd_list = [
+        "ac_config set -m eweb_password '{}'".format(pass_str),
+        'uci set luci.main.loginNum="0"',
+        "uci commit"
+    ]
+    params['console']['send_string'] = '\r\n'.join(cmd_list)
     params['console']['format'] = 'str'
     params['console']['wait'] = 1
+    time.sleep(1)
     return params
 
 
 def h1(params):
+    time.sleep(1)
     return params
 
 
