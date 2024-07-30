fb808cc7f078b3e30a52a3bde36be0009c37f554
Ebenezer Scrooge <1641367382@qq.com>
2024-07-30 10:13:19 +0800

    Update example_use_wd.py

--- 408/OS/2024/ModularAutomation/example_use_wd.py
+++ 408/OS/2024/ModularAutomation/example_use_wd.py
@@ -10,14 +10,14 @@ if __name__ == '__main__':
     params = fetch(params=params)
     if len(list(params.keys())) <= 0:
         params = {
-            'working': 'payload',
+            'todo': 'todo',
             'watchdog': {
                 'port': 20291,
                 'exe_file': 'example_use_wd.exe'
             }
         }
     fc = FlowChart(prerequisite=params)
-    fc.load_map(hook_script='hook_script.py', map_json='flowchart_json.pos')
+    fc.load_map(hook_script='todo.py', map_json='todo.pos')
     end = False
     while not end:
         end = fc.run_step()
