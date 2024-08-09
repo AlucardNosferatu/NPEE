fecedaf09630855ac7578e0c6f9b8ceaa493875a
Ebenezer Scrooge <1641367382@qq.com>
2024-08-09 09:30:01 +0800

    Update

--- 408/OS/2024/ModularAutomation/safe_common_config.py
+++ 408/OS/2024/ModularAutomation/safe_common_config.py
@@ -1,8 +1,8 @@
-scan_host = '10.51.132.74'
+scan_host = '10.51.132.2'
 scan_host_web = 'http://{}/'.format(scan_host)
-target_name = 'BE68-ULTRA'
-serial_number = 'MACCR06747685'
-eweb_pass = 'Ruijie123'
-ssh_pass = '6a398294d40cda39'
+target_name = 'H20M'
+serial_number = 'G1RUAGA000075'
+eweb_pass = 'LostXmas20291224'
+ssh_pass = 'e5b4577287972a54'
 filename_bin = 'EW_3.0(1)B11P262_BE68PRO_11193009_install_encypto.bin'
 folder = r'C:\Users\16413\Desktop\Job\努力搬砖\安全扫描\国内\BE68-ULTRA-R262'
