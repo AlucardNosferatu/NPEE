1c46ab93551f1a91f8dc7434234cd2510638229b
Ebenezer Scrooge <1641367382@qq.com>
2025-05-28 10:22:04 +0800

    Update CheckCode.py

--- 408/Organization/BaseAndCode/CheckCode.py
+++ 408/Organization/BaseAndCode/CheckCode.py
@@ -78,7 +78,8 @@ if __name__ == '__main__':
     hc, pb = hamming_code_check(data_bits, cdp, gl)
     cdp_ask = random.randint(0, len(cdp) - 1)
     # 生成问题
-    question = f"数据位{data_bits}，校验位p{cdp_ask + 1}的覆盖位置是？p{cdp_ask + 1}等于多少？"
-    answer = f"覆盖位置：{sorted(gl[cdp[cdp_ask]])}，计算得p{cdp_ask + 1}={pb[cdp_ask]}"
+    question = f"数据位（从高到低）{data_bits}，校验位p{cdp_ask + 1}的覆盖位置是？p{cdp_ask + 1}等于多少？"
+    answer = f"覆盖位置（下标从1开始）：{sorted(gl[cdp[cdp_ask]])}，计算得p{cdp_ask + 1}={pb[cdp_ask]}，校验位（从高到低）：{pb}" \
+             f"\n完整数据（混入校验，从低到高）：{hc}\n完整数据（校验在最后，从高到低）：{data_bits + pb}，"
     print(question)
     print(answer)
