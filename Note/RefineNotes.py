ad49d589d8bcbd06036fa97965fb82dc975a9463
Ebenezer Scrooge <1641367382@qq.com>
2025-12-12 08:35:55 +0800

    Update

--- Note/RefineNotes.py
+++ Note/RefineNotes.py
@@ -9,13 +9,13 @@ if __name__ == '__main__':
     # 准备真题问题列表
     pkl_files = glob.glob(os.path.join('日期', "**", "*.pkl"), recursive=True)
     # 假设这是您已有的RAG结果
-    analyzer = IncrementalDateAnalyzer(top_k=10, memory_limit=100)
+    analyzer = IncrementalDateAnalyzer(top_k=250, memory_limit=250)
     for pkl_file in pkl_files:
         with open(pkl_file, "rb") as f:
             # 3. 调用dump，把字典写入文件
             rag_result = pickle.load(f)
             analyzer.update(rag_result)
-    top_k = analyzer.get_top_dates_df(top_n=10)['日期'].to_list()
+    top_k = analyzer.get_top_dates_df(top_n=20)['日期'].to_list()
     copy_sheets_simple(
         "C:\\Users\\16413\\Desktop\\NPEE\\统计\\WWII\\工作日志.xlsx",
         "工作摘要.xlsx",
