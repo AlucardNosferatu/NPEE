ca44e8c27b3d407930e045a35e2fee2cb1cd7645
Ebenezer Scrooge <1641367382@qq.com>
2026-03-16 10:04:41 +0800

    Update my_dbg.py

--- FZU/PlanB/Practice/Transfer-Assembly/ReverseEngineer/my_dbg.py
+++ FZU/PlanB/Practice/Transfer-Assembly/ReverseEngineer/my_dbg.py
@@ -5,19 +5,27 @@ k32 = windll.kernel32
 # 设置 LoadLibraryA
 k32.LoadLibraryA.argtypes = [c_char_p]
 k32.LoadLibraryA.restype = HMODULE  # HMODULE 在 my_dbg_def 中定义为 c_void_p
+LoadLibraryA = k32.LoadLibraryA
+
 k32.GetModuleHandleA.restype = HMODULE
+GetModuleHandleA = k32.GetModuleHandleA
+
 k32.CloseHandle.argtypes = [HMODULE]
+CloseHandle = k32.CloseHandle
 # 设置 GetProcAddress
 k32.GetProcAddress.argtypes = [HMODULE, c_char_p]
 k32.GetProcAddress.restype = c_void_p
+GetProcAddress = k32.GetProcAddress
 
 # 设置 GetModuleFileNameA
 k32.GetModuleFileNameA.argtypes = [HMODULE, c_char_p, DWORD]
 k32.GetModuleFileNameA.restype = DWORD
+GetModuleFileNameA = k32.GetModuleFileNameA
 
 # 设置 FreeLibrary（你后面会用到）
 k32.FreeLibrary.argtypes = [HMODULE]
 k32.FreeLibrary.restype = c_bool  # 如果没定义 BOOL，可以用 c_bool
+FreeLibrary = k32.FreeLibrary
 
 # 设置 ReadProcessMemory
 k32.ReadProcessMemory.argtypes = [
@@ -28,6 +36,7 @@ k32.ReadProcessMemory.argtypes = [
     POINTER(c_size_t)  # lpNumberOfBytesRead
 ]
 k32.ReadProcessMemory.restype = c_bool
+ReadProcessMemory = k32.ReadProcessMemory
 
 # 设置 WriteProcessMemory
 k32.WriteProcessMemory.argtypes = [
@@ -38,6 +47,20 @@ k32.WriteProcessMemory.argtypes = [
     POINTER(c_size_t)  # lpNumberOfBytesWritten
 ]
 k32.WriteProcessMemory.restype = c_bool
+WriteProcessMemory = k32.WriteProcessMemory
+
+CreateProcessA = k32.CreateProcessA
+OpenProcess = k32.OpenProcess
+DebugActiveProcess = k32.DebugActiveProcess
+WaitForDebugEvent = k32.WaitForDebugEvent
+ContinueDebugEvent = k32.ContinueDebugEvent
+SetThreadContext = k32.SetThreadContext
+DebugActiveProcessStop = k32.DebugActiveProcessStop
+OpenThread = k32.OpenThread
+CreateToolhelp32Snapshot = k32.CreateToolhelp32Snapshot
+Thread32First = k32.Thread32First
+Thread32Next = k32.Thread32Next
+GetThreadContext = k32.GetThreadContext
 
 
 class Debugger:
@@ -55,7 +78,7 @@ class Debugger:
         data = b""
         read_buf = create_string_buffer(length)
         count = c_ulonglong(0)
-        if not k32.ReadProcessMemory(self.h_process, addr, read_buf, length, byref(count)):
+        if not ReadProcessMemory(self.h_process, addr, read_buf, length, byref(count)):
             return None
         else:
             data += read_buf.raw
@@ -65,7 +88,7 @@ class Debugger:
         count = c_ulonglong(0)
         length = len(data)
         c_data = c_char_p(data)
-        if not k32.WriteProcessMemory(self.h_process, addr, c_data, length, byref(count)):
+        if not WriteProcessMemory(self.h_process, addr, c_data, length, byref(count)):
             return False
         else:
             return True
@@ -76,7 +99,7 @@ class Debugger:
                 original_byte = self.read_process_mem(addr, 1)
                 print('original_byte', original_byte)
                 if not self.write_process_mem(addr=addr, data=b"\xCC"):
-                    print('Cannot write process memory, err code:', k32.GetLastError())
+                    print('Cannot write process memory, err code:', GetLastError())
                     return False
                 else:
                     test_byte = self.read_process_mem(addr, 1)
@@ -89,9 +112,9 @@ class Debugger:
 
     @staticmethod
     def resolve_dll_func(dll_name, func_name):
-        handle = k32.GetModuleHandleA(dll_name.encode('utf-8'))
-        addr = k32.GetProcAddress(handle, func_name.encode('utf-8'))
-        k32.CloseHandle(handle)
+        handle = GetModuleHandleA(dll_name.encode('utf-8'))
+        addr = GetProcAddress(handle, func_name.encode('utf-8'))
+        CloseHandle(handle)
         print(func_name, '的地址是', hex(addr))
         return addr
 
@@ -104,7 +127,7 @@ class Debugger:
         startup_info.wShowWindow = 0x1
 
         startup_info.cb = sizeof(startup_info)
-        if k32.CreateProcessA(
+        if CreateProcessA(
                 path_to_exe,
                 None,
                 None,
@@ -120,11 +143,11 @@ class Debugger:
             print('PID:', process_info.dwProcessId)
 
         else:
-            print('ERR:', k32.GetLastError())
+            print('ERR:', GetLastError())
 
     @staticmethod
     def open_process(pid):
-        h_process = k32.OpenProcess(
+        h_process = OpenProcess(
             PROCESS_ALL_ACCESS,
             False,
             pid
@@ -133,7 +156,7 @@ class Debugger:
 
     def attach(self, pid):
         self.h_process = self.open_process(pid)
-        if k32.DebugActiveProcess(pid):
+        if DebugActiveProcess(pid):
             self.dbg_active = True
             self.pid = int(pid)
         else:
@@ -142,16 +165,16 @@ class Debugger:
     def run(self):
         while self.dbg_active:
             self.get_debug_event()
-            cmd = input('input quit to stop.')
-            if cmd.lower() in ['quit']:
-                self.dbg_active = False
+            # cmd = input('input quit to stop.')
+            # if cmd.lower() in ['quit']:
+            #     self.dbg_active = False
 
     def get_debug_event(self):
         debug_event = DEBUG_EVENT()
         continue_status = DBG_CONTINUE
-        if k32.WaitForDebugEvent(byref(debug_event), INFINITE):
+        if WaitForDebugEvent(byref(debug_event), INFINITE):
             if self.h_thread is not None:
-                k32.CloseHandle(self.h_thread)
+                CloseHandle(self.h_thread)
             self.h_thread = self.open_thread(tid=debug_event.dwThreadId)
             self.context = self.get_thread_ctx(h_thread=self.h_thread)
             print(
@@ -172,7 +195,7 @@ class Debugger:
             else:
                 if self.exception_code is not None:
                     print('非预期的异常，代码:', self.exception_code)
-            k32.ContinueDebugEvent(
+            ContinueDebugEvent(
                 debug_event.dwProcessId,
                 debug_event.dwThreadId,
                 continue_status
@@ -187,11 +210,11 @@ class Debugger:
             # original byte and then set the thread's context record
             # with the new EIP value
             self.context.Rip = self.exception_addr
-            k32.SetThreadContext(self.h_thread, byref(self.context))
+            SetThreadContext(self.h_thread, byref(self.context))
         return DBG_CONTINUE
 
     def detach(self):
-        if k32.DebugActiveProcessStop(self.pid):
+        if DebugActiveProcessStop(self.pid):
             print('Finished')
             return True
         else:
@@ -200,7 +223,7 @@ class Debugger:
 
     @staticmethod
     def open_thread(tid):
-        h_thread = k32.OpenThread(THREAD_ALL_ACCESS, None, tid)
+        h_thread = OpenThread(THREAD_ALL_ACCESS, None, tid)
         if h_thread is None:
             print('Fail to obtain thread:', tid)
         return h_thread
@@ -208,15 +231,15 @@ class Debugger:
     def enum_threads(self):
         thread_entry = THREADENTRY32()
         threads_list = []
-        snapshot = k32.CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, self.pid)
+        snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, self.pid)
         if snapshot is not None:
             thread_entry.dwSize = sizeof(thread_entry)
-            success = k32.Thread32First(snapshot, byref(thread_entry))
+            success = Thread32First(snapshot, byref(thread_entry))
             while success:
                 if thread_entry.th32OwnerProcessID == self.pid:
                     threads_list.append(thread_entry.th32ThreadID)
-                success = k32.Thread32Next(snapshot, byref(thread_entry))
-            k32.CloseHandle(snapshot)
+                success = Thread32Next(snapshot, byref(thread_entry))
+            CloseHandle(snapshot)
             return threads_list
         else:
             print('snapshot is None')
@@ -227,7 +250,7 @@ class Debugger:
         ctx.ContextFlags = CONTEXT_FULL | CONTEXT_DEBUG_REGISTERS
         if h_thread is None:
             h_thread = self.open_thread(tid=tid)
-        if k32.GetThreadContext(h_thread, byref(ctx)):
+        if GetThreadContext(h_thread, byref(ctx)):
             return ctx
         else:
             print('Fail to get thread contex')
