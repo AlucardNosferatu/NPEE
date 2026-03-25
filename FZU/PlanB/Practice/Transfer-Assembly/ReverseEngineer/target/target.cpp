37d3e78a5de1cb8d7417903b54dab0ae7d5578bd
Ebenezer Scrooge <1641367382@qq.com>
2026-03-25 20:58:52 +0800

    Update

--- FZU/PlanB/Practice/Transfer-Assembly/ReverseEngineer/target/target.cpp
+++ FZU/PlanB/Practice/Transfer-Assembly/ReverseEngineer/target/target.cpp
@@ -3,8 +3,9 @@
 #pragma comment(lib, "user32.lib")
 
 // ✅ 关键：导出 + 防止C++名字修饰
-extern "C" __declspec(dllexport)
-__declspec(noinline) void test_func()
+// extern "C" __declspec(dllexport)
+// __declspec(noinline) void test_func()
+void test_func()
 {
     printf("Target activated, PID = %u\n", GetCurrentProcessId());
     printf("test_func address (inside): %p\n", test_func);
